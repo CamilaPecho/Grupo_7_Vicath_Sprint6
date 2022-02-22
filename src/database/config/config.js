278f0988d6be98ca2b70a46b17dd74f16be0e613
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2022-02-22 01:36:57 -0300

    Todos - Generando BD remota v9

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,7 +7,8 @@ module.exports = {
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  use_env_variables: "mysql://cqxvh50z5e0acwaf:b9y5yz3tac2qif52@dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hcudqlwlba0y5v4b"
   }
 
 }
