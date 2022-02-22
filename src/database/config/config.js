83a64bc46faa4df5601d093c17e1f93046708b5f
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2022-02-22 01:39:54 -0300

    Todos - Generando BD remota v10

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,14 +1,31 @@
 require('dotenv').config()
-module.exports = {
-  
-  "development": {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  use_env_variables: "mysql://cqxvh50z5e0acwaf:b9y5yz3tac2qif52@dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hcudqlwlba0y5v4b"
-  }
+
+module.exports =
+
+{
+    "development": {
+
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+
+    // seederStorage: "sequelize",
+    // seederStorageTableName: "seeds",
+
+    // migrationStorage: "sequelize",
+    // migrationStorageTableName: "migrations"
+},
+    "production": {
+        
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
+}
 
 }
